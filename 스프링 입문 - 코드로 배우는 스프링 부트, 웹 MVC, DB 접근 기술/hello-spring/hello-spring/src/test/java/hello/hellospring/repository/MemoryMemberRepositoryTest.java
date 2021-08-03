625add7587f78f18fca3e38180152706622f982c
NyeongB <kdtoaj14@ajou.ac.kr>
2021-08-03 21:44:43 +0900

    Test Case

---
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/hello-spring/src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,68 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+//mport org.junit.jupiter.api.*;
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        // 일종의 콜백
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spirng");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        //System.out.println("result = " + (result == member));
+        //Assertions.assertEquals(member, result);
+        //Assertions.assertEquals(member, null);
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("SPRING1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("SPRING2");
+        repository.save(member2);
+
+        Member result = repository.findByNmae("SPRING1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("SPRING1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("SPRING2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        //assertThat(result.size()).isEqualTo(3);
+
+    }
+}
