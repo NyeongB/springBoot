3a25b540c103275169a63672bbfd128b571c5056
NyeongB <kdtoaj14@ajou.ac.kr>
2021-08-05 20:26:50 +0900

    Component Scan

--- 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ 스프링 입문 - 코드로 배우는 스프링 부트, 웹 MVC, DB 접근 기술/hello-spring/hello-spring/src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 // ctrl + shift + T
 class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    /*MemberService memberService = new MemberService();
 
 
     @Test
@@ -39,19 +39,19 @@ class MemberServiceTest {
 
         //when
         memberService.join(member1);
-        
 
- /*       try {
+
+ *//*       try {
             memberService.join(member2);
             fail("");
         }catch (IllegalStateException e){
             assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-        }*/
+        }*//*
 
 
 
         //then
-    }
+    }*/
 
     @Test
     void findMembers() {
