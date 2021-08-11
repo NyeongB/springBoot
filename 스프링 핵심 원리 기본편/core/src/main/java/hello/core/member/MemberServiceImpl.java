2ccffbdaef9cecfca4ddd3b40c2530d166c6466e
NyeongB <kdtoaj14@ajou.ac.kr>
2021-08-11 21:32:05 +0900

    회원 도메인 실행과 테스트

---
+++ 스프링 핵심 원리 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+
+    private MemberRepository memberRepository = new MemoryMemberRepository();
+
+    // 멤버서비스 구현체가 멤버리포지토리 추상화에도 의존, 메모리멤버리포지토리인 구현체에도 의존
+    // OCP 위배, DIP 위배 좋은 코드는 아니다.
+
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
