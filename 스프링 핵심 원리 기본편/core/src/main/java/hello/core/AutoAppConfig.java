60e1256cdc94f655e9b450b3669712b88035a774
NyeongB <kdtoaj14@ajou.ac.kr>
2021-08-21 11:35:50 +0900

    필터 : include, exclude 지정

--- 스프링 핵심 원리 기본편/core/src/main/java/hello/core/AutoAppConfig.java
+++ 스프링 핵심 원리 기본편/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        //basePackages = "hello.core.member",
+        //basePackageClasses = AutoAppConfig.class,
+        // 지정하지않으면 얘의 패키지부터 다 뒤진다.
         excludeFilters =  @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
